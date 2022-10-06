e24ced8d30437e8e9e161aec42e27a05fe235f20
Arthur Edmond Jenkins <deesrex@fox5.co.za>
2022-10-06 18:03:12 +0200

    Upload and Download

--- src/app/services/ipfs.service.ts
+++ src/app/services/ipfs.service.ts
@@ -7,11 +7,36 @@ export class IpfsService {
 
   constructor() { }
 
-  downloadFile(CID:string) {
+  async downloadFile(CID: string) {
     //insert code to get the file from tatum IPFS endpoint
+    const id = CID;
+    const resp = await fetch(`https://api-eu1.tatum.io/v3/ipfs/${id}`, {
+      method: 'GET',
+      headers: {
+        'x-api-key': 'd7dd4818-7e3b-4656-81ad-05a06b6aa4a3'
+      }
+    });
+
+
+    const blob = await resp.blob();
+    console.log(blob);
+
+
+    const data = await resp.text();
+    console.log(data);
   }
 
-  uploadFile(file:any){
+  async uploadFile(file: any) {
     //insert code to post file to IPFS tatum
+    const id = file.name.split('.')[0];
+    return await fetch(
+      `https://api-eu1.tatum.io/v3/ipfs/${id}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-api-key': 'a529e629-e787-4e3d-aac8-f02aa7f4f257'
+        }
+      }
+    );
   }
 }
